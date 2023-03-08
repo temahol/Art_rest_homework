b4e1b3a74cd1849f7415de3c0414606e9f2fdd71
Artem <aholevko@maxilect.com>
2023-03-08 23:31:28 +0300

    add allure

--- src/test/java/RestTests.java
+++ src/test/java/RestTests.java
@@ -1,33 +1,54 @@
 import helpers.TestBase;
 import io.restassured.response.Response;
+import model.lombok.TestBaseLombok;
+import model.lombok.TestBaseLombokResponse;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.*;
+import static helpers.CustomAllureListener.withCustomTemplates;
 
-public class RestTests extends TestBase {
+public class RestTests {
 
     @Test
     void createNewUser() {
-        given()
+
+        TestBaseLombok testLombok = new TestBaseLombok();
+        testLombok.setCreateUser(testLombok.createUserBody);
+        testLombok.setBaseUrl(testLombok.baseUrl);
+        testLombok.setCreateUser(testLombok.createUser);
+
+        TestBaseLombokResponse lombokResponse = given()
+                .filter(withCustomTemplates())
                 .when()
-                .body(createUserBody)
-                .post(baseUrl + createUser)
+                .body(testLombok.createUserBody)
+                .post(testLombok.baseUrl + testLombok.createUser)
                 .then()
                 .statusCode(201)
                 .log().body()
                 .body("$", hasKey("id"))
-                .body("$", hasKey("createdAt"));
+                .body("$", hasKey("createdAt"))
+                .extract().as(TestBaseLombokResponse.class);
+
+        assertThat(lombokResponse.getId()).isNotNull();
+        assertThat(lombokResponse.getCreatedAt()).isNotNull();
     }
 
     @Test
     void updateUser() {
+
+        TestBaseLombok testLombok = new TestBaseLombok();
+        testLombok.setUpdateUserBody(testLombok.updateUserBody);
+        testLombok.setBaseUrl(testLombok.baseUrl);
+        testLombok.setPutUpdateUser(testLombok.putUpdateUser);
+
         given()
+                .filter(withCustomTemplates())
                 .when()
-                .body(updateUserBody)
-                .put(baseUrl + putUpdateUser)
+                .body(testLombok.updateUserBody)
+                .put(testLombok.baseUrl  + testLombok.putUpdateUser)
                 .then()
                 .statusCode(200)
                 .log().all()
@@ -36,9 +57,15 @@ public class RestTests extends TestBase {
 
     @Test
     void deleteUser() {
+
+        TestBaseLombok testLombok = new TestBaseLombok();
+        testLombok.setBaseUrl(testLombok.baseUrl);
+        testLombok.setDeleteUser(testLombok.deleteUser);
+
         given()
+                .filter(withCustomTemplates())
                 .when()
-                .delete(baseUrl + deleteUser)
+                .delete(testLombok.baseUrl + testLombok.deleteUser)
                 .then()
                 .statusCode(204)
                 .log().all();
@@ -46,9 +73,15 @@ public class RestTests extends TestBase {
 
     @Test
     void checkTotal() {
+
+        TestBaseLombok testLombok = new TestBaseLombok();
+        testLombok.setBaseUrl(testLombok.baseUrl);
+        testLombok.setListOfUsers(testLombok.listOfUsers);
+
         given()
+                .filter(withCustomTemplates())
                 .when()
-                .get(baseUrl + listOfUsers)
+                .get(testLombok.baseUrl + testLombok.listOfUsers)
                 .then()
                 .statusCode(200)
                 .log().all()
@@ -58,9 +91,15 @@ public class RestTests extends TestBase {
 
     @Test
     void checkAllLastNames() {
+
+        TestBaseLombok testLombok = new TestBaseLombok();
+        testLombok.setBaseUrl(testLombok.baseUrl);
+        testLombok.setListOfUsers(testLombok.listOfUsers);
+
         given()
+                .filter(withCustomTemplates())
                 .when()
-                .get(baseUrl + listOfUsers)
+                .get(testLombok.baseUrl + testLombok.listOfUsers)
                 .then()
                 .statusCode(200)
                 .log().all()
--- src/test/java/helpers/TestBase.java
+++ src/test/java/helpers/TestBase.java
@@ -10,4 +10,36 @@ public class TestBase {
     public String singleResource = "/api/unknown/2";
     public String createUserBody = "{ \"name\": \"test-user\", \"job\": \"test-job\" }";
     public String updateUserBody = "{ \"name\": \"update-user\", \"job\": \"update-job\" }";
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public void setListOfUsers(String listOfUsers) {
+        this.listOfUsers = listOfUsers;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public void setPutUpdateUser(String putUpdateUser) {
+        this.putUpdateUser = putUpdateUser;
+    }
+
+    public void setDeleteUser(String deleteUser) {
+        this.deleteUser = deleteUser;
+    }
+
+    public void setSingleResource(String singleResource) {
+        this.singleResource = singleResource;
+    }
+
+    public void setCreateUserBody(String createUserBody) {
+        this.createUserBody = createUserBody;
+    }
+
+    public void setUpdateUserBody(String updateUserBody) {
+        this.updateUserBody = updateUserBody;
+    }
 }
